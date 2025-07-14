1e59aac97e608e87908d98c5e21727c97f477160
User++ <ilatolstopatov79@gmail.com>
2025-07-15 04:01:03 +0800

    Added information transmission
    Now you can transfer data to the server and so on the contrary

--- source/client.d
+++ source/client.d
@@ -2,7 +2,28 @@ import std.socket;
 import std.stdio;
 import std.conv;
 import std.string;
+import core.thread;
 
+
+void receiveMessages(Socket client)
+{
+    char[1024] buffer;
+
+    while(true)
+    {
+        long received = client.receive(buffer);
+
+        if(received <= 0)
+        {
+            break;
+        }
+
+        writeln("\n[Server]: ", cast(string) buffer[0..received].strip());
+        write("You: ");
+
+        stdout.flush();
+    }
+}
 
 void main()
 {
@@ -11,11 +32,25 @@ void main()
     clientSocket.connect(new InternetAddress("127.0.0.1", 8080));
     writeln("Connected to server");
 
-    clientSocket.send("Hello from client!");
+    new Thread(() => receiveMessages(clientSocket)).start(); 
 
-    char[] buffer;
-    long bytesRead = clientSocket.receive(buffer);
-    writeln("Received from server: ", cast(string) buffer[0 .. bytesRead]);
+    while (true)
+    {
+        write("You: ");
+
+        stdout.flush();
+
+        string msg = readln().strip();
+
+        if (msg == "exit")
+        {
+            break;
+        }
+
+        clientSocket.send(msg);
+    }
 
     clientSocket.close();
+
+    writeln("Disconnected from the server");
 }
--- source/server.d
+++ source/server.d
@@ -1,18 +1,64 @@
 import std.socket;
 import std.stdio;
+import std.string;
+import std.conv;
+import core.thread;
 
+
+void receiveMessages(Socket client)
+{
+    char[1024] buffer;
+
+    while(true)
+    {
+        long received = client.receive(buffer);
+
+        if(received <= 0)
+        {
+            break;
+        }
+
+        writeln("\n[Client]: ", cast(string) buffer[0..received].strip());
+        write("You: ");
+
+        stdout.flush();
+    }
+}
 
 void main()
 {
     Socket serverSocket = new TcpSocket();
     serverSocket.bind(new InternetAddress("127.0.0.1", 8080));
-    serverSocket.listen(10);
+    serverSocket.listen(3);
 
     writeln("Server is created!");
+    writeln("Waiting for a client");
 
     Socket clientSocket = serverSocket.accept();
+
+    writeln("The client connected");
     writeln("Connection established with ", clientSocket.remoteAddress);
 
-    clientSocket.send("Hello from server!");
+    new Thread(() => receiveMessages(clientSocket)).start(); 
+
+    while (true)
+    {
+        write("You: ");
+
+        stdout.flush();
+
+        string msg = readln().strip();
+
+        if (msg == "exit")
+        {
+            break;
+        }
+
+        clientSocket.send(msg);
+    }
+
     clientSocket.close();
+    serverSocket.close();
+
+    writeln("The server is stopped");
 }
